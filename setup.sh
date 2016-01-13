7e2cd0eb0caa649785c76d124fdf313054319375
Adrian Farmadin <adrian.farmadin@gepardec.com>
2016-01-13 10:46:11 +0100

    Diagrams

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ node1 configure config
 node1 deploy invmhttp.war
 [ ! -f "~/.node2rc" ] && ./bin/setup.sh -i node2 -j $HOME/node2 -o 100 -d $(pwd) -r wildfly-10.0.0.CR5    
 node2 configure config
-node2 deploy invmother.war
+node2 deploy client.war
